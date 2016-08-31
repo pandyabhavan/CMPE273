e8bf84b5f4906033d5a79095bff27f0dfdbf8cac
pandyabhavan <pandyabhavan134@gmail.com>
2016-08-31 16:13:32 -0700

    Project Created

---
+++ homework1/src/main/java/com/cmpe273/homework1/App.java
@@ -0,0 +1,13 @@
+package com.cmpe273.homework1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
